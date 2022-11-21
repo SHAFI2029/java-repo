0277afa08a7922ad0d0f8cdab747593bd225d53f
SHAFI2029 <117617551+SHAFI2029@users.noreply.github.com>
2022-11-21 20:29:05 +0530

    Update original.java

--- original.java
+++ original.java
@@ -1,5 +1,6 @@
 class Solution{
   public static void main(String []args){
-     System.out.println("this is 1st modification file");
+     System.out.println("this is 2st modification file");
+     System.out.println("this is 2nd file");
      }
 }
